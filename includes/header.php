2136e83e302935c67e69cf6906292eb37751b115
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-05-10 18:14:27 +0000

    Adding GA - fixes #1

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,18 @@
     <link rel="stylesheet" type="text/css" href="/presentations/assets/css/presentations.css" />
 </head>
 <body>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-40821033-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+
     <div class="navbar navbar-static-top">
       <div class="navbar-inner">
         <span class="title">Jonathan Klein - <?=$title?></a>
