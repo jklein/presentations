f7ac735552ffbc9ec20a316efb5869e02e46a394
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-05-10 19:00:00 +0000

    Adding content to the jsday page

--- jsday.php
+++ jsday.php
@@ -5,7 +5,65 @@ include 'includes/header.php';
 ?>
 
 <div class="container">
+    <div style="text-align:center">
+        <iframe src="http://www.slideshare.net/slideshow/embed_code/20940383" width="683" height="570"
+            frameborder="0" marginwidth="0" marginheight="0" scrolling="no"
+            style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px"
+            allowfullscreen webkitallowfullscreen mozallowfullscreen>
+        </iframe>
+        <div style="margin-bottom:5px">
+            <strong>
+                <a href="http://www.slideshare.net/jnklein/jsday-2013-practical-responsive-web-design"
+                    title="JSDay 2013 - Practical Responsive Web Design" target="_blank">
+                    JSDay 2013 - Practical Responsive Web Design
+                </a>
+            </strong> from
+            <strong><a href="http://www.slideshare.net/jnklein" target="_blank">Jonathan Klein</a></strong>
+        </div>
+    </div>
 
+    <div class="resources row">
+        <div class="span12">
+            <h2>Resources</h2>
+        </div>
+
+        <div class="span6">
+            <h5>Image Compression</h5>
+            <ul>
+                <li><a href="http://www.smushit.com/ysmush.it/">SmushIt</a></li>
+                <li><a href="https://developers.google.com/speed/pagespeed/">PageSpeed</a></li>
+                <li><a href="https://github.com/mikebrittain/Wesley">Wesley (command line tool)</a></li>
+            </ul>
+            <h5>Picturefill</h5>
+            <ul>
+                <li><a href="https://github.com/scottjehl/picturefill">Github Project</a></li>
+                <li><a href="http://scottjehl.github.io/picturefill/">Demo Page</a></li>
+            </ul>
+            <h5>Handling Old IE</h5>
+            <ul>
+                <li><a href="http://adactio.com/journal/4494/">Conditional Comments</a></li>
+                <li><a href="http://buildmobile.com/understanding-responsive-web-design-cross-browser-compatibility/">Understanding Cross Browser Compatibility</a></li>
+
+            </ul>
+        </div>
+        <div class="span6">
+            <h5>Responsive Images</h5>
+            <ul>
+                <li><a href="http://blog.cloudfour.com/responsive-imgs/">Responsive Images Part 1</a></li>
+                <li><a href="http://blog.cloudfour.com/responsive-imgs-part-2/">Responsive Images Part 2</a></li>
+                <li><a href="http://blog.cloudfour.com/responsive-imgs-part-3-future-of-the-img-tag/">Responsive Images Part 3</a></li>
+                <li><a href="http://blog.cloudfour.com/8-guidelines-and-1-rule-for-responsive-images/">8 Guidelines and 1 Rule</a></li>
+                <li><a href="http://blog.cloudfour.com/sensible-jumps-in-responsive-image-file-sizes/">Sensible Jumps in File Size</a></li>
+                <li><a href="http://adaptive-images.com/">Adaptive-images</a></li>
+            </ul>
+            <h5>Other Links</h5>
+            <ul>
+                <li><a href="https://github.com/igrigorik/http-client-hints/">Client Hints</a></li>
+                <li><a href="http://www.etsy.com/careers">Etsy Careers</a></li>
+                <li><a href="http://www.jonathanklein.net">My Blog</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 
 <?php
