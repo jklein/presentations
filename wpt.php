d67afcdb3b34dd849c4eaebf8335ba49de639c1f
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-10-07 20:07:12 +0000

    Adding the file for my WPT talk

---
+++ wpt.php
@@ -0,0 +1,60 @@
+<?php
+
+$title = "Velocity New York 2013 - DIY Synthetic: Private WebPagetest Magic";
+include 'includes/header.php';
+?>
+
+<div class="container">
+    <div style="text-align:center">
+        <iframe src="http://www.slideshare.net/jnklein/foo" width="683" height="570"
+            frameborder="0" marginwidth="0" marginheight="0" scrolling="no"
+            style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px"
+            allowfullscreen webkitallowfullscreen mozallowfullscreen>
+        </iframe>
+        <div style="margin-bottom:5px">
+            <strong>
+                <a href="#"
+                    title="DIY Synthetic: Private WebPagetest Magic" target="_blank">
+                    Velocity New York 2013 - DIY Synthetic: Private WebPagetest Magic
+                </a>
+            </strong> from
+            <strong><a href="http://www.slideshare.net/jnklein" target="_blank">Jonathan Klein</a></strong>
+        </div>
+    <!--<h4><a href="http://joind.in/talk/view/8595">Rate this talk!</a></h4>-->
+    </div>
+
+    <div class="resources row">
+        <div class="span12">
+            <h2>Resources</h2>
+        </div>
+
+        <div class="span6">
+            <h5>WebPagetest</h5>
+            <ul>
+                <li><a href="http://www.webpagetest.org/">Public Instance</a></li>
+                <li><a href="https://github.com/WPO-Foundation/webpagetest">GitHub</a></li>
+                <li><a href="https://sites.google.com/a/webpagetest.org/docs/">Docs</a></li>
+                <li><a href="https://sites.google.com/a/webpagetest.org/docs/using-webpagetest/metrics">Metrics Monitored</a></li>
+                <li><a href="http://www.webpagetest.org/forums/">Forums</a></li>
+                <li><a href="https://sites.google.com/a/webpagetest.org/docs/private-instances#TOC-EC2-Test-Agents">EC2 Test Agents</a></li>
+            </ul>
+        </div>
+        <div class="span6">
+            <h5>Etsy Tools</h5>
+            <ul>
+                <li><a href="https://github.com/etsy/wpt-script">wpt-script</a></li>
+                <li><a href="https://github.com/etsy/statsd">StatsD</a></li>
+                <li><a href="https://github.com/etsy/dashboard">Etsy Dashboards</a></li>
+            </ul>
+            <h5>Other Tools</h5>
+            <ul>
+                <li><a href="https://github.com/marcelduran/webpagetest-api">webpagetest-api</a></li>
+                <li><a href="http://www.catchpoint.com/">Catchpoint</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<?php
+
+include 'includes/footer.php';
