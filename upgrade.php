5d19c1e71970dad36c5964cb1795198d887cb1d9
Jonathan Klein <jonathan.n.klein@gmail.com>
2013-11-11 17:23:39 +0000

    Adding upgrading the web presentation page

---
+++ upgrade.php
@@ -0,0 +1,64 @@
+<?php
+
+$title = "Velocity London 2013 - Upgrading the Web: Driving Support for New Standards";
+include 'includes/header.php';
+?>
+
+<div class="container">
+    <div style="text-align:center">
+        <!-- Script tag for Speakerdeck goes here-->
+        <div style="margin-bottom:5px">
+            <strong>
+                <a href="#"
+                    title="Upgrading the Web: Driving Support for New Standards" target="_blank">
+                    Velocity London 2013 - Upgrading the Web: Driving Support for New Standards
+                </a>
+            </strong> from
+            <strong><a href="https://speakerdeck.com/jklein" target="_blank">Jonathan Klein</a></strong>
+        </div>
+    </div>
+
+    <div class="resources row">
+        <div class="span12">
+            <h2>Resources</h2>
+        </div>
+
+        <div class="span6">
+            <h5>HTTP Origin/Hop Hints</h5>
+            <ul>
+                <li><a href="http://tools.ietf.org/html/draft-nottingham-http-browser-hints-05">Draft Spec</a></li>
+            </ul>
+            <h5>Client Hints</h5>
+            <ul>
+                <li><a href="https://github.com/igrigorik/http-client-hints/blob/master/draft-grigorik-http-client-hints-01.txt">Draft Spec</a></li>
+                <li><a href="http://www.igvita.com/2013/08/29/automating-dpr-switching-with-client-hints/">Article About Implementation</a></li>
+            </ul>
+            <h5>WebP</h5>
+            <ul>
+                <li><a href="http://www.jonathanklein.net/2013/02/a-comprehensive-guide-to-webp.html">Comprehensive Guide</a></li>
+                <li><a href="http://www.webpagetest.org/result/131105_H0_13GQ/">WebPagetest Before</a></li>
+                <li><a href="http://www.webpagetest.org/result/131105_SS_13K7/">WebPagetest After</a></li>
+            </ul>
+        </div>
+        <div class="span6">
+            <h5>SPDY/HTTP 2.0</h5>
+            <ul>
+                <li><a href="http://en.wikipedia.org/wiki/SPDY">About SPDY</a></li>
+                <li><a href="https://developers.google.com/speed/spdy/">Google Documentation</a></li>
+                <li><a href="http://www.webpagetest.org/result/131105_SS_13K7/">WebPagetest Before</a></li>
+                <li><a href="http://www.webpagetest.org/result/131105_B1_13QT/">WebPagetest After</a></li>
+            </ul>
+
+            <h5>Prefetching</h5>
+            <ul>
+                <li><a href="http://www.stevesouders.com/blog/2013/11/07/prebrowsing/">Steve Souders on "Prebrowsing"</a></li>
+                <li><a href="http://www.webpagetest.org/result/131105_8Z_14X3/">WebPagetest Before</a></li>
+                <li><a href="http://www.webpagetest.org/result/131105_8V_15EW/">WebPagetest After</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<?php
+
+include 'includes/footer.php';
