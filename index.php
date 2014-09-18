a1550c3cb1fa3eac91f1b911788ad57093e20d6d
Jonathan Klein <jonathan.n.klein@gmail.com>
2014-09-18 17:55:27 -0400

    More refactoring, removing dead files

--- index.php
+++ index.php
@@ -7,16 +7,31 @@ $app = new \Slim\Slim();
 $view = $app->view();
 $view->setTemplatesDirectory(__DIR__ . '/templates');
 
-$app->get('/', function () use ($app) {
-    $talks_data = json_decode(file_get_contents('data.json'), true);
+$app->talks_data = json_decode(file_get_contents('data.json'), true);
 
+$app->get('/', function () use ($app) {
     $template_data = [
         'title' => 'All Talks',
-        'talks_data' => $talks_data,
+        'talks_data' => $app->talks_data,
     ];
 
     $app->render('talk_index.php', $template_data);
 });
 
+// Now automatically generate all of the routes
+foreach ($app->talks_data as $time => $talks) {
+    foreach ($talks as $title => $data) {
+        if (preg_match('/^\/[a-zA-Z0-9\_]+$/', $data['page_link']) === 1) {
+            $app->get($data['page_link'], function () use ($app, $title, $time) {
+                $template_data = [
+                    'title' => $title,
+                    'data' => $app->talks_data[$time][$title],
+                ];
+
+                $app->render('talk.php', $template_data);
+            })->name(substr($data['page_link'], 1));
+        }
+    }
+}
 
 $app->run();
--- templates/talk.php
+++ templates/talk.php
@@ -1,6 +1,54 @@
 <?php
 
 include 'includes/header.php';
+?>
+
+<div class="container">
+    <div class="row" style="text-align:center">
+        <div class="col-md-1"></div>
+        <div class="col-md-10" style="margin:auto">
+            <?php
+            if (!empty($data['video_link'])) { ?>
+                <div class="col-md-12">
+                    <h4><a href="<?=$data['video_link'];?>">Watch the Video Here</a></h4>
+                </div>
+            <?php
+            } ?>
+            <script async class="speakerdeck-embed" data-id="<?=$data['speakerdeck_id']?>" data-ratio="<?=$data['speakerdeck_ratio']?>" src="//speakerdeck.com/assets/embed.js"></script>
+            <div style="margin-bottom:5px">
+                <strong>
+                    <a href="<?=$data['slides_link']?>" title="<?=$title?>" target="_blank">
+                        <?=$title;?>
+                    </a>
+                </strong> from
+                <strong><a href="https://speakerdeck.com/jklein" target="_blank">Jonathan Klein</a></strong><br />
+            </div>
+            <h2><a href="<?=$data['rate_link']?>">Rate this talk!</a></h2>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+
+    <div class="resources row">
+        <div class="col-md-12">
+            <h2>Resources</h2>
+        </div>
+
+        <?php
+        foreach ($data['links'] as $column => $group) {
+            echo '<div class="col-md-6">';
+            foreach ($group as $heading => $links) {
+                echo '<h5>' . $heading . '</h5>';
+                echo '<ul>';
+                foreach ($links as $href => $title) {
+                    echo '<li><a href="' . $href . '">' . $title . '</a></li>';
+                }
+                echo '</ul>';
+            }
+            echo '</div>';
+        }?>
+    </div>
+</div>
 
 
+<?php
 include 'includes/footer.php';
